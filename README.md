9196a497a37b915bd47d58f3cdda6f4acab280c7
maxime <maxime.montcourant@sfr.fr>
2023-04-28 19:54:30 +0200

    xsd-gen 1st version

---
+++ README.md
@@ -0,0 +1,31 @@
+# xsd-gen README
+
+## Features
+
+generate xml scheme in xsd file.
+
+> Tip: Use with Ctrl + Alt + x
+
+## Requirements
+
+Windows only.
+
+## Extension Settings
+
+This extension contributes the following settings:
+
+* `xsd-gen.enable`: Enable/disable this extension.
+
+## Known Issues
+
+No known issues at this time.
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
+
+### 1.0.0
+
+Initial release of XSD Generator
+
+**Enjoy!**
---
+++ extension.js
@@ -0,0 +1,71 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const { exec } = require('child_process');
+const vscode = require('vscode');
+const path = require('path');
+const fs = require('fs');
+
+function runJar(context) {
+	const activeEditor = vscode.window.activeTextEditor;
+	if (!activeEditor) {
+		vscode.window.showInformationMessage('Aucun fichier ouvert dans l\'éditeur');
+		return;
+	}
+
+	const filePath = activeEditor.document.uri.fsPath;
+	if (path.extname(filePath) !== '.xml') {
+		vscode.window.showInformationMessage('Le fichier doit être un fichier XML');
+		return;
+	}
+
+	const fileName = path.parse(filePath).name;
+	const fileDir = path.parse(filePath).dir;
+	const extPath = context.extensionPath;
+
+	// exec('dir', (error, stdout, stderr) => {
+	// 	if (error) {
+	// 		console.error('Erreur: \\' + fileName);
+	// 		vscode.window.showErrorMessage('Erreur dir: ' + error.message);
+	// 		return;
+	// 	}
+
+	// 	console.error('stdout: ' + fileDir);
+	// });
+
+	exec('java -jar ' + extPath + '\\jars\\xsd-gen-fat-0.2.4.jar ' + fileDir + '\\' + fileName + '.xml > ' + fileDir + '\\' + fileName + '.xsd', (error, stdout, stderr) => {
+		if (error) {
+			console.error('Erreur: ' + error.message);
+			vscode.window.showErrorMessage('Erreur lors de la génération du fichier XSD: ' + error.message);
+			return;
+		}
+
+		console.log('stdout: ' + stdout);
+		// console.error('stderr: ' + stderr);
+		vscode.window.showInformationMessage('Fichier XSD généré avec succès : ' + fileName + '.xsd');
+	});
+}
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	console.log('Congratulations, your extension "xsd-gen" is now active!');
+
+	let disposable = vscode.commands.registerCommand('xsd-gen.generateXsd', function () {
+		runJar(context)
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
